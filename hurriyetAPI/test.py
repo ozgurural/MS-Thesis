b32dfffd679fb9b560e2a4e748a91ee32f90500d
ozgur <e181962@metu.edu.tr>
2018-12-27 00:23:34 +0300

    Hurriyet api added

---
+++ hurriyetAPI/test.py
@@ -0,0 +1,12 @@
+from hurriyetApi import HurriyetApi
+
+apikey = "58147a8ca752485fad3cf28e5e35a87d"
+api = HurriyetApi(apikey)
+writer = api.searchWriter("Adil")
+articles = api.listArticles(42, 3)
+searchKeyword = api.search("siber")
+
+print(searchKeyword)
+
+with open('hurriyet.html', 'w', encoding='utf-8') as the_file:
+	the_file.write(str(searchKeyword))
